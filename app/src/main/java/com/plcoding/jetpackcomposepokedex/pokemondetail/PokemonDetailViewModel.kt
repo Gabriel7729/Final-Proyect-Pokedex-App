2e72a0fcc6d1f4927e73e08789027b465cbe949e
Gabriel7729 <Lusan7729@gmail.com>
2021-08-15 10:52:15 -0400

    Initial proyecto with List Pokemon Completled

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemondetail/PokemonDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.plcoding.jetpackcomposepokedex.pokemondetail
+import androidx.lifecycle.ViewModel
+import com.plcoding.jetpackcomposepokedex.data.remote.response.Pokemon
+import com.plcoding.jetpackcomposepokedex.repository.PokemonRepository
+import com.plcoding.jetpackcomposepokedex.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/util/PokemonParse.kt
@@ -0,0 +1,7 @@
+package com.plcoding.jetpackcomposepokedex.util
+import androidx.compose.ui.graphics.Color
+import com.plcoding.jetpackcomposepokedex.data.remote.response.Type
+import com.plcoding.jetpackcomposepokedex.data.remote.response.Stat
+import com.plcoding.jetpackcomposepokedex.ui.theme.*
+import java.util.*
+
