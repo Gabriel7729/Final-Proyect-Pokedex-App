726b587ab11fea4e7d6b056e1d5b291dfa89d366
Ronald123ol <70978284+Ronald123ol@users.noreply.github.com>
2021-08-16 19:20:50 -0400

    Diseño-Estadisticas-Pokemon
    -Se agrega el modelo PokemonDetailViewModel y PokemonDetailScreen para la vista.

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemondetail/PokemonDetailViewModel.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemondetail/PokemonDetailViewModel.kt
@@ -6,3 +6,13 @@ import com.plcoding.jetpackcomposepokedex.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+
+@HiltViewModel
+class PokemonDetailViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        return repository.getPokemonInfo(pokemonName)
+    }
+}
